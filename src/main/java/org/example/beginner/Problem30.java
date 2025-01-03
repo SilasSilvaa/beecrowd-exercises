f62e4e98fa785beae14d0f68b34fb4248e67ae88
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2025-01-02 22:49:35 -0300

    BEE 1060 - positive numbers

---
+++ src/main/java/org/example/beginner/Problem30.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+/*
+Faça um programa que leia 6 valores.
+Estes valores serão somente negativos ou positivos (desconsidere os valores nulos).
+A seguir, mostre a quantidade de valores positivos digitados.
+
+Entrada
+Seis valores, negativos e/ou positivos.
+
+Saída
+Imprima uma mensagem dizendo quantos valores positivos foram lidos.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float num1 = scanner.nextFloat();
+        float num2 = scanner.nextFloat();
+        float num3 = scanner.nextFloat();
+        float num4 = scanner.nextFloat();
+        float num5 = scanner.nextFloat();
+        float num6 = scanner.nextFloat();
+        int quantity = 0;
+
+        var listNumbers = List.of(num1, num2, num3, num4, num5, num6);
+
+        for(float num : listNumbers ){
+            if(num > 0){
+                quantity += 1;
+            }
+        }
+        System.out.println(quantity + " valores positivos");
+    }
+}
